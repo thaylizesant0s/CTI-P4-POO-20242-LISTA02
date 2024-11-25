9ac23705ca6bed7ae1570a46defd063ff4f38065
Thaylize Santos <161877793+thaylizesant0s@users.noreply.github.com>
2024-11-25 06:29:25 -0300

    Questões de JAVA

--- CAP05/EXERCICIOS-RESOLVIDOS/EXE18/src/br/edu/principal/Principal.java
+++ CAP05/EXERCICIOS-RESOLVIDOS/EXE18/src/br/edu/principal/Principal.java
@@ -1,10 +1,27 @@
 package br.edu.principal;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        double num, quad, cubo, raiz;
 
-	}
+        System.out.print("Digite um número: ");
+        num = sc.nextDouble();
 
+        while (num > 0) {
+            quad = num * num;
+            cubo = num * num * num;
+            raiz = Math.sqrt(num);
+
+            System.out.println("Quadrado: " + quad);
+            System.out.println("Cubo: " + cubo);
+            System.out.println("Raiz Quadrada: " + raiz);
+
+            System.out.print("Digite um número: ");
+            num = sc.nextDouble();
+        }
+    }
 }
